099bdd2e1cf85e5f4938abdfb25ce4e3770ed1d7
Alejandro Hahn <halexhg@gmail.com>
2020-02-23 22:43:44 -0700

    Completed Lab 3

---
+++ src/main/java/mx/tec/lab/usingmocks/UsingmocksApplication.java
@@ -0,0 +1,13 @@
+package mx.tec.lab.usingmocks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsingmocksApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UsingmocksApplication.class, args);
+    }
+
+}
